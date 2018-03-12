d2cc296ddaa639e268b7aa49119b71847310b32b
nicolas erfe <nicolas.erfe@gmail.com>
2018-03-12 19:51:48 +0200

    Population task uploaded

---
+++ Population/City.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Population
+{
+    public class City
+    {
+        string name;
+        int population;
+
+        City(string name, int population)
+        {
+            this.name = name;
+            this.population = population;
+        }
+    }
+}
---
+++ Population/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Population
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<City> citiesList = new List<City>();
+            string listOfCities = "Kharkiv=1431000,Kiev=2804000,Las Vegas=603400";
+            string[] dividors = { "," };
+            string[] cities = listOfCities.Split(dividors, StringSplitOptions.RemoveEmptyEntries);
+            int maxCityNameLength = 0;
+            int maxCityPopulation = 0;
+            foreach(var city in cities)
+            {
+                string[] innerDividors = { "=" };
+                int dividorPosition = city.IndexOf(innerDividors[0]);
+                var cityName = city.Substring(0, dividorPosition);
+                if(cityName.Length > maxCityNameLength)
+                {
+                    maxCityNameLength = cityName.Length;
+                }
+                var cityPopulation = city.Substring(dividorPosition + 1, city.Length - dividorPosition - 1);
+                Console.WriteLine($"CityName: "+cityName);
+                Console.WriteLine($"CityPopulation: "+cityPopulation);
+                //citiesList.Add(cityName, cityPopulation);
+                string[] cityParameters = city.Split(innerDividors, StringSplitOptions.RemoveEmptyEntries);
+            }
+            //Console.WriteLine($"Longest name: "maxCityNameLength);
+            Console.ReadKey();
+        }
+
+        
+    }
+}
